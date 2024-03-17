6e401eff66e2944b4690503fcd7d0244b5bcf62f
Eiko Thomas <eiko.thomas@gmail.com>
2024-03-17 20:03:34 +0100

    add zellij and lazyvim

---
+++ nvim/lua/plugins/plantuml.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "tyru/open-browser.vim",
+    lazy = false,
+  },
+  {
+    "aklt/plantuml-syntax",
+    ft = { "plantuml", "puml" },
+  },
+  {
+    "weirongxu/plantuml-previewer.vim",
+    ft = { "plantuml", "puml" },
+  },
+}

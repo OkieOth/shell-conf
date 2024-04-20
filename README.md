b3353e0059e4c55d0f393a92911f4b6137c5f734
Eiko Thomas <eiko.thomas@swarco.com>
2024-04-20 22:42:17 +0200

    add bat and update fish

---
+++ README.md
@@ -0,0 +1,13 @@
+# Additional tools
+* fish shell
+* zellij
+* bat (https://github.com/sharkdp/bat)
+* eza (https://github.com/eza-community/eza/blob/main/INSTALL.md)
+
+## Requirements
+
+* node installed
+* golang installed
+* rust installed
+* nvim installed
+
--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,40 @@ if status --is-login
     set -gx PATH $PATH ~/.local/bin
 end
 
+set -g fish_greeting
+
 function ssh_pwd --wraps ssh --description 'alias for ssh with enforced password auth'
     ssh -o PubkeyAuthentication=no -o PreferredAuthentications=password $argv
+end
+
+function cat --wraps cat --description 'alias to wrap cat with bat'
+    bat $argv
+end
+
+function gdb --wraps git --description 'alias wrapper to combine git diff with bat'
+    git diff --name-only --relative --diff-filter=d | xargs bat --diff
+end
+
+function ll
+    eza -l $argv
+end
+
+function la
+    eza -l -a $argv
+end
+
+function lld
+    eza -l -D $argv
+end
+
+function lad
+    eza -l -a -D $argv
+end
+
+function lltd
+    eza --tree -D $argv
+end
+
+function llt
+    eza --tree -L 3 $argv
 end
